d0440de168ef9b469c6fd2d407c4e1245aabef45
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-02-19 13:46:05 +0700

    🧱 Setup Gradle build tool

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun property(name: String) = project.findProperty(name)?.toString()
+
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "1.1.0"
+    signing
+}
+
+group = property("group")!!
+version = property("version")!!
+description = property("description")
+
+val jdkVersion = "17"
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(jdkVersion))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useKotlinTest(embeddedKotlinVersion)
+        }
+
+        val functionalTest by registering(JvmTestSuite::class) {
+            useKotlinTest(embeddedKotlinVersion)
+
+            dependencies {
+                implementation(project())
+            }
+
+            targets {
+                all {
+                    testTask.configure { shouldRunAfter(test) }
+                }
+            }
+        }
+    }
+}
+
+gradlePlugin {
+    testSourceSets(sourceSets["functionalTest"])
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = jdkVersion
+        targetCompatibility = jdkVersion
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = jdkVersion
+    }
+
+    named("check") {
+        dependsOn(testing.suites.named("functionalTest"))
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mcdev-gradle-plugin"
