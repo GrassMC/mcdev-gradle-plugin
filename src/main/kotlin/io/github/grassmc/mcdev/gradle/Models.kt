1f5e4cb6e27230d4306b4a0bfce3af5df2af09a4
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-02-23 20:19:18 +0700

    🍻 Add common repository extensions

---
+++ src/main/kotlin/io/github/grassmc/mcdev/gradle/Models.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 GrassMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.grassmc.mcdev.gradle
+
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.kotlin.dsl.maven
+
+internal data class RepositoryHolder(val name: String, val url: String) {
+    fun configure(handler: RepositoryHandler, action: MavenArtifactRepository.() -> Unit = {}) =
+        handler.maven(url) {
+            name = this@RepositoryHolder.name
+            action()
+        }
+}
