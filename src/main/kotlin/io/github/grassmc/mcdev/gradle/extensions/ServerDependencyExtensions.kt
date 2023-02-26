5d7aa1e7cb554f178315f89e01acfd65faba7ec7
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-02-26 15:00:47 +0700

    ♻️ Add function to getting api notation for each server vendor

--- src/main/kotlin/io/github/grassmc/mcdev/gradle/extensions/ServerDependencyExtensions.kt
+++ src/main/kotlin/io/github/grassmc/mcdev/gradle/extensions/ServerDependencyExtensions.kt
@@ -20,62 +20,68 @@ import io.github.grassmc.mcdev.gradle.version.MinecraftVersion
 import io.github.grassmc.mcdev.gradle.version.MinecraftVersion.*
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
-private object ServerDependencies {
-    const val R0_1_SUFFIX = "-R0.1"
+internal object ServerDependencies {
+    private const val R0_1_SUFFIX = "-R0.1"
 
-    val SPIGOT_API = DependencyHolder("org.spigotmc", "spigot-api", true, R0_1_SUFFIX)
+    private val SPIGOT_API = DependencyHolder("org.spigotmc", "spigot-api", true, R0_1_SUFFIX)
 
     // For version 1.17 or later
-    val PAPER_API = DependencyHolder("io.papermc.paper", "paper-api", true, R0_1_SUFFIX)
+    private val PAPER_API = DependencyHolder("io.papermc.paper", "paper-api", true, R0_1_SUFFIX)
 
     // For version 1.16.5 or older
-    val PAPER_API_LEGACY = DependencyHolder("com.destroystokyo.paper", "paper-api", true, R0_1_SUFFIX)
+    private val PAPER_API_LEGACY = DependencyHolder("com.destroystokyo.paper", "paper-api", true, R0_1_SUFFIX)
 
     // For version 1.8.8
-    val PAPER_API_1_8_8 = DependencyHolder("org.github.paperspigot", "paperspigot-api", true, R0_1_SUFFIX)
+    private val PAPER_API_1_8_8 = DependencyHolder("org.github.paperspigot", "paperspigot-api", true, R0_1_SUFFIX)
 
-    val PURPUR_API = DependencyHolder("org.purpurmc.purpur", "purpur-api", true, R0_1_SUFFIX)
+    private val PURPUR_API = DependencyHolder("org.purpurmc.purpur", "purpur-api", true, R0_1_SUFFIX)
+
+    fun spigotApiNotation(version: MinecraftVersion): String {
+        check(version != V1_8_1 || version != V1_8_2 || version != V1_8_9) {
+            "Please use spigot-api Minecraft version $V1_8_8 instead of $version!"
+        }
+        check(version != V1_9_1 || version != V1_9_3) {
+            "Please use paper-api Minecraft version $V1_9_4 instead of $version!"
+        }
+        check(version != V1_10_1) { "Please use paper-api Minecraft version $V1_10_2 instead of $version!" }
+
+        return SPIGOT_API.notation(version.asString())
+    }
+
+    fun paperApiNotation(version: MinecraftVersion): String {
+        val dependency = when (version) {
+            V1_8_9,
+            in V1_8..V1_8_7,
+            -> throw IllegalArgumentException("Please use paperspigot-api Minecraft version $V1_8_8 instead of $version!")
+
+            in V1_9..V1_9_3,
+            -> throw IllegalArgumentException("Please use paper-api Minecraft version $V1_9_4 instead of $version!")
+
+            V1_10,
+            V1_10_1,
+            -> throw IllegalArgumentException("Please use paper-api Minecraft version $V1_10_2 instead of $version!")
+
+            V1_8_8 -> PAPER_API_1_8_8
+            in V1_11..V1_16_5 -> PAPER_API_LEGACY
+            else -> PAPER_API
+        }
+        return dependency.notation(version.asString())
+    }
+
+    fun purpurApiNotation(version: MinecraftVersion): String {
+        check(version <= V1_16_5) { "purpur-api doesn't support versions older than Minecraft 1.16.5!" }
+        return PURPUR_API.notation(version.asString())
+    }
 }
 
-fun DependencyHandler.spigotApi(version: MinecraftVersion): String {
-    check(version != V1_8_1 || version != V1_8_2 || version != V1_8_9) {
-        "Please use spigot-api Minecraft version $V1_8_8 instead of $version!"
-    }
-    check(version != V1_9_1 || version != V1_9_3) {
-        "Please use paper-api Minecraft version $V1_9_4 instead of $version!"
-    }
-    check(version != V1_10_1) { "Please use paper-api Minecraft version $V1_10_2 instead of $version!" }
-
-    return ServerDependencies.SPIGOT_API.notation(version.asString())
-}
+fun DependencyHandler.spigotApi(version: MinecraftVersion): String = ServerDependencies.spigotApiNotation(version)
 
 fun DependencyHandler.spigotApi(version: String) = spigotApi(MinecraftVersion.matching(version))
 
-fun DependencyHandler.paperApi(version: MinecraftVersion): String {
-    val dependency = when (version) {
-        V1_8_9,
-        in V1_8..V1_8_7,
-        -> throw IllegalArgumentException("Please use paperspigot-api Minecraft version $V1_8_8 instead of $version!")
-
-        in V1_9..V1_9_3,
-        -> throw IllegalArgumentException("Please use paper-api Minecraft version $V1_9_4 instead of $version!")
-
-        V1_10,
-        V1_10_1,
-        -> throw IllegalArgumentException("Please use paper-api Minecraft version $V1_10_2 instead of $version!")
-
-        V1_8_8 -> ServerDependencies.PAPER_API_1_8_8
-        in V1_11..V1_16_5 -> ServerDependencies.PAPER_API_LEGACY
-        else -> ServerDependencies.PAPER_API
-    }
-    return dependency.notation(version.asString())
-}
+fun DependencyHandler.paperApi(version: MinecraftVersion): String = ServerDependencies.paperApiNotation(version)
 
 fun DependencyHandler.paperApi(version: String) = paperApi(MinecraftVersion.matching(version))
 
-fun DependencyHandler.purpurApi(version: MinecraftVersion): String {
-    check(version <= V1_16_5) { "purpur-api doesn't support versions older than Minecraft 1.16.5!" }
-    return ServerDependencies.PURPUR_API.notation(version.asString())
-}
+fun DependencyHandler.purpurApi(version: MinecraftVersion): String = ServerDependencies.purpurApiNotation(version)
 
 fun DependencyHandler.purpurApi(version: String) = purpurApi(MinecraftVersion.matching(version))
